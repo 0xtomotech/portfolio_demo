27482a98bc9bece2215fe4522ca6752062afa005
0xtomotech <0xtomotech@gmail.com>
2024-05-05 13:51:32 +0200

    Adjusted About.tsx to escape apostrophe

--- components/About.tsx
+++ components/About.tsx
@@ -20,39 +20,40 @@ const About = () => {
       <SectionHeading>About Me & My Sister</SectionHeading>
 
       <p className="mb-12">
-        Around here, life is never dull! We might tussle over who's the boss of
-        the toy basket, but when it comes to cuddle time, we're a team. Whether
-        we're plotting to snag some treats or racing to claim the coziest lap,
-        we're always in sync—even when we're in a feisty mood!
+        Around here, life is never dull! We might tussle over who&apos;s the
+        boss of the toy basket, but when it comes to cuddle time, we&apos;re a
+        team. Whether we&apos;re plotting to snag some treats or racing to claim
+        the coziest lap, we&apos;re always in sync—even when we&apos;re in a
+        feisty mood!
       </p>
 
       <p className="mb-12">
         Our days are filled with mischief and play, but nothing compares to our
         pre-walk jitters. I, Rizsi, get so excited I could spin just like
-        Gabner's head spins! And Popi, well, she's there, wagging along,
-        probably dreaming about adevnutres with her parner in crime, Tomo, and
-        oh boy, they are a legendary duo. After our adventures, it's all about
-        the snuggles—
+        Gabner&apos;s head spins! And Popi, well, she&apos;s there, wagging
+        along, probably dreaming about adventures with her partner in crime,
+        Tomo, and oh boy, they are a legendary duo. After our adventures,
+        it&apos;s all about the snuggles—
         <span className="font-medium">
-          if you've got the lap, we've got the love.
+          if you&apos;ve got the lap, we&apos;ve got the love.
         </span>{" "}
         Mono is our favourite human pillow!
       </p>
 
       <p className="mb-12">
-        But let's talk fears—thunderstorms turn me, Popi, into a nervous wreck,
-        hiding under the first toilet I can find. Rizsi tries to play it cool,
-        often distracted by those sneaky cats outside. It's tough being brave,
-        but together, we manage, turning our home into a fortress of comfort on
-        stormy days.
+        But let&apos;s talk fears—thunderstorms turn me, Popi, into a nervous
+        wreck, hiding under the first toilet I can find. Rizsi tries to play it
+        cool, often distracted by those sneaky cats outside. It&apos;s tough
+        being brave, but together, we manage, turning our home into a fortress
+        of comfort on stormy days.
       </p>
 
       <p className="">
         On the bright side, sunbathing is our shared hobby, alongside a shared
-        mission of keeping those cats and ducks in check. It’s a tough job, but
-        hey, someone’s got to do it! Together, we’re ready for anything—fun,
-        frolic, or the occasional fright. Our life? It’s a tail-wagging tale of
-        adventure and affection!
+        mission of keeping those cats and ducks in check. It&apos;s a tough job,
+        but hey, someone&apos;s got to do it! Together, we&apos;re ready for
+        anything—fun, frolic, or the occasional fright. Our life? It&apos;s a
+        tail-wagging tale of adventure and affection!
       </p>
     </motion.section>
   );
